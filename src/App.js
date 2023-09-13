28b740d8191bbe145bd6d4f7b4e8a9fe360b0dcb
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-09-13 18:40:29 +0300

    router structure installed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import { Header } from "components";
+import { Home, Login, Register } from "pages";
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
