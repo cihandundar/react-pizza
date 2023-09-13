b8e92e699d8b37522070344ea4c648ebf3aedb0b
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-09-13 21:27:13 +0300

    home page fix

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-
+// import Kopernik from "../../assets/images/kopernik.gif";
+import { PizzaItem } from "components";
 const Home = () => {
   const navigate = useNavigate();
   const { user } = useSelector((state) => state.auth);
@@ -12,7 +13,18 @@ const Home = () => {
     }
   }, [user, navigate]);
 
-  return <div>Home</div>;
+  return (
+    <main className="main">
+      <section className="section">
+        {/* <div className="section__img">
+          <img src={Kopernik} alt="" />
+        </div> */}
+        <div className="section__content">
+          <PizzaItem />
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default Home;
