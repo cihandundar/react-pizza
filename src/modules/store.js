3f606257463b1c4c081c90c730f4fe32e7449e8f
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-09-13 21:26:57 +0300

    pizza reducer imported

--- src/modules/store.js
+++ src/modules/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 import authReducer from "../features/auth/authSlice";
-
+import pizzaReducer from "../features/pizza/pizzaSlice";
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    pizza: pizzaReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
