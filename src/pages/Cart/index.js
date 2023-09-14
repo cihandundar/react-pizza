200b0ff0cd20a1d89924e8046ea4c0e1b7c79efc
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-09-14 13:46:15 +0300

    cart page edited

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,65 @@
+import {
+  addToCart,
+  clearCart,
+  decreaseCart,
+  remove,
+} from "features/cart/cartSlice";
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  const items = useSelector((state) => state?.cart.cartItems);
+  const dispatch = useDispatch();
+
+  const handleRemove = (info) => {
+    dispatch(remove(info));
+  };
+  const handleDecreaseCart = (info) => {
+    dispatch(decreaseCart(info));
+  };
+
+  const handleIncreaseCart = (info) => {
+    dispatch(addToCart(info));
+  };
+  const handleClearCart = () => {
+    dispatch(clearCart());
+  };
+
+  return (
+    <div>
+      {items.length === 0 ? (
+        <div className="empty">
+          <h4>Your cart is empty. Start shopping now!</h4>
+        </div>
+      ) : (
+        <ul className="row">
+          {items.map((info) => (
+            <li className="cart" key={info.id}>
+              <div className="cart__img">
+                <img src={info.image_url} alt="" />
+              </div>
+              <div className="cart__title">
+                <h3>{info.title}</h3>
+              </div>
+              <div className="cart__quantity">
+                <button onClick={() => handleDecreaseCart(info)}>-</button>
+                <div className="cart__quantity__count">
+                  <h4>{info.cartQuantity}</h4>
+                </div>
+                <button onClick={() => handleIncreaseCart(info)}>+</button>
+              </div>
+              <button className="remove" onClick={() => handleRemove(info)}>
+                Remove
+              </button>
+            </li>
+          ))}
+          <button className="clear" onClick={() => handleClearCart()}>
+            Clear Cart
+          </button>
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default Cart;
