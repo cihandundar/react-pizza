c98189dada4387e8572a79fe7da83f0e43896da5
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-09-14 13:46:04 +0300

    cart slice page imported

--- src/modules/store.js
+++ src/modules/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 import authReducer from "../features/auth/authSlice";
 import pizzaReducer from "../features/pizza/pizzaSlice";
+import cartReducer from "../features/cart/cartSlice";
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     pizza: pizzaReducer,
+    cart: cartReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
