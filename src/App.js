b7806fd8fb149f6fe2b0f30bdaf326509310acce
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-09-14 13:44:57 +0300

    toastify added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import { Header } from "components";
 import { Cart, Home, Login, Register } from "pages";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
     <BrowserRouter>
+      <ToastContainer position="bottom-right" />
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
