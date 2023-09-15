79535caa6162a26360e3e9299152d4c0faffb42e
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-09-15 19:03:37 +0300

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,4 +2,5 @@ import Home from "./Home";
 import Login from "./Login";
 import Register from "./Register";
 import Cart from "./Cart";
-export { Home, Login, Register, Cart };
+import PizzaDetails from "./PizzaDetails";
+export { Home, Login, Register, Cart, PizzaDetails };
